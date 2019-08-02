979c39503e54612e64a69c694be9d3daee838345
Akatsuki <33372019+raaapiw@users.noreply.github.com>
2019-08-02 09:57:29 +0700

    fix faculty

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -141,8 +141,8 @@
                 <div class="card-body little-profile text-center">
                     <div class="pro-img"><img src="{{ asset('material/images/users/profile.png')}}" alt="user" /></div>
                     <h3 class="m-b-0">{{$user->name}}</h3>
-                    <p>FRI - 1201150444</p>
-                    <p>{{$user->reviews}} points, {{$user->reviews}} Review</p>
+                    <p>{{$row->faculty}} - {{$row->id}}</p>
+                    <p>{{$user->points}} points, {{$user->reviews}} Review</p>
                 </div>
             </div>
         </div>
